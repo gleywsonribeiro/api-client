6d7bdd8ee0ef2e6475fd87440d8a37bf68440024
Gleywson Ribeiro <gleywson@gmail.com>
2022-12-12 22:55:56 -0300

    Created findById resource.

--- src/main/java/br/com/client/api/resources/ClientResource.java
+++ src/main/java/br/com/client/api/resources/ClientResource.java
@@ -3,7 +3,9 @@ package br.com.client.api.resources;
 import br.com.client.api.entities.Client;
 import br.com.client.api.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +20,10 @@ public class ClientResource {
     @GetMapping
     public List<Client> findAll() {
         return clientService.findPaged();
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Client> clientById(@PathVariable Long id) {
+        Client client = clientService.findById(id);
+        return ResponseEntity.ok().body(client);
     }
 }
--- src/main/java/br/com/client/api/services/ClientService.java
+++ src/main/java/br/com/client/api/services/ClientService.java
@@ -2,11 +2,13 @@ package br.com.client.api.services;
 
 import br.com.client.api.entities.Client;
 import br.com.client.api.repositories.ClientRepository;
+import br.com.client.api.resources.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClientService {
@@ -16,5 +18,11 @@ public class ClientService {
     @Transactional(readOnly = true)
     public List<Client> findPaged() {
         return repository.findAll();
+    }
+
+    public Client findById(Long id) {
+        Optional<Client> optional = repository.findById(id);
+        Client client = optional.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+        return client;
     }
 }
